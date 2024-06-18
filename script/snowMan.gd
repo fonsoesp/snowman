125a883cda3281ac5d833fe5e5e4828211ce41e9
Pablo <pablocalatayudd@gmail.com>
2024-06-18 13:26:57 +0200

    Cambios level1 y nivel perdido

--- script/snowMan.gd
+++ script/snowMan.gd
@@ -1,6 +1,5 @@
 extends RigidBody2D
 
-@export var game_over_label : Label
 @export var reset_delay = 2.0
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +11,6 @@ func _on_body_entered(body):
 		_bounce()
 	elif body.is_in_group("Enemigo"):
 		_game_over()
-		print("Dead")
 	elif body.is_in_group("Salida"):
 		print("Salida")
 
@@ -21,9 +19,8 @@ func _bounce():
 	pass
 	
 func _game_over():
-	# Mostrar el mensaje de Game Over
+	$"../GameOverLabel".visible = true
 	
-	# Esperar unos segundos y luego resetear el nivel
 	var timer = Timer.new()
 	timer.wait_time = reset_delay
 	timer.one_shot = true
@@ -33,17 +30,6 @@ func _game_over():
 
 func _reset_level():
 	get_tree().reload_current_scene()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _integrate_forces(state):
-	# Detecta colisiones
-	#if 
-	#for i in range(get_contact_count()):
-		#var contact = get_contact_collider_object(i)
-		#if contact.is_in_group("platform"):  # Verifica si el cuerpo en contacto pertenece al grupo "platform"
-			## Resetea la velocidad vertical y aplica la fuerza del salto
-			#linear_velocity.y = 0
-			#apply_central_impulse(Vector2(0, jump_force))
-			#break
 
 
 
