890555650976cb109c19fc2511b9ec7c42994264
Pablo <pablocalatayudd@gmail.com>
2024-06-18 13:51:39 +0200

    El jugador gana y pierde

--- script/snowMan.gd
+++ script/snowMan.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 @export var reset_delay = 2.0
+var dead = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,9 +11,10 @@ func _on_body_entered(body):
 	if body.is_in_group("PlayerBar"):
 		_bounce()
 	elif body.is_in_group("Enemigo"):
-		_game_over()
+		if !dead:
+			_game_over()
 	elif body.is_in_group("Salida"):
-		print("Salida")
+		_win()
 
 func _bounce():
 	set_linear_velocity(Vector2(get_linear_velocity().x,-500))
@@ -20,6 +22,8 @@ func _bounce():
 	
 func _game_over():
 	$"../GameOverLabel".visible = true
+	dead = true
+	$Sprite2D/AnimationPlayer.play("death")
 	
 	var timer = Timer.new()
 	timer.wait_time = reset_delay
@@ -27,6 +31,9 @@ func _game_over():
 	add_child(timer)
 	timer.timeout.connect(_reset_level)
 	timer.start()
+
+func _win():
+	$"../WinLabel".visible = true;
 
 func _reset_level():
 	get_tree().reload_current_scene()
